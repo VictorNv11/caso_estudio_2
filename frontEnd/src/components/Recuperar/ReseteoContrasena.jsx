af3b7c57fe151bb1ff1c3caa3fb64715564ad5c2
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-03-04 14:39:16 -0500

    Manejo de errores y validaciones en el módulo de servicios

--- frontEnd/src/components/Recuperar/ReseteoContrasena.jsx
+++ frontEnd/src/components/Recuperar/ReseteoContrasena.jsx
@@ -1,44 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '..//..//assets/img/planetas.png';
 import img_home from "..//..//assets/img/img_home.png";
-import { Link, } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import queryString from 'query-string';
 import axios from "axios";
 
-
 export const ReseteoContrasena = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const location = useLocation();
 
-    // state={};
+    useEffect(() => {
+        const { email, token } = queryString.parse(location.search);
+        // Hacer algo con el correo electrónico y el token, si es necesario
+    }, [location.search]);
 
-    // handleSubmit = e =>{
-    //     e.preventDefault();
-
-    //     const data={
-    //         token:this.props.match.params.id,
-    //         password:this.password,
-    //         password_confirm:this.password_confirm
-    //     };
-
-    //     axios.post('reset', data).then(
-    //         res=>{
-    //             console.log(res);
-    //             this.setState({
-    //                 reset:true
-    //             }
-    //             )
-    //         }
-    //     ).catch(
-    //         err=>{
-    //             console.log(err);
-    //         }
-    //     )
-        
-
-    // }
-
-    // if(this.state.reset){
-    //     return <Redirect to={'/login'}></Redirect>
-
-    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { email, token } = queryString.parse(location.search);
+          const response = await axios.post('http://localhost:8000/password/reset', {
+            email: email,
+            token: token,
+            password: password,
+            password_confirmation: confirmPassword
+          });
+          setMessage(response.data.message);
+        } catch (error) {
+          setMessage('Hubo un error al restablecer la contraseña.');
+        }
+      };
+    
 
   return (
         <section className="vh-100 gradient-custom"  style={{ backgroundImage: `url(${img_home})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center center' }}>
@@ -67,27 +60,40 @@ export const ReseteoContrasena = () => {
                 <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
                     <div className="card-body p-5 text-center">
                         <div className="mb-md-5 mt-md-4 pb-5">
-       <form>
+       <form onSubmit={handleSubmit}>
         <h3>Resetear Contraseña</h3>
-    
-        <div className="input-group">
-                                        <input
-                                            className="form-control form-control-lg"
-                                            placeholder='ingresa tu nueva contraseña'
-                                            onChange={(e) => this.password=e.target.value}
-                                        />
-                                        <label className="form-label" htmlFor="typePasswordX">Contraseña</label>
+ 
+        <div className="form-group">
+                                            <label className="form-label" htmlFor="password">Nueva Contraseña:</label>
+                                            <input
+                                                className="form-control form-control-lg"
+                                                placeholder='Ingresa tu nueva contraseña'
+                                                type="password"
+                                                id="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                required
+                                            />
+                                        </div>
 
-                                        <input
-                                            className="form-control form-control-lg"
-                                            placeholder='ingresa tu nueva contraseña'
-                                            onChange={(e) =>this.password_confirm=e.target.value}
-                                        />
+                                        <div className="form-group">
+                                            <label className="form-label" htmlFor="confirmPassword">Confirmar Contraseña:</label>
+                                            <input
+                                                className="form-control form-control-lg"
+                                                placeholder='Confirma tu nueva contraseña'
+                                                type="password"
+                                                id="confirmPassword"
+                                                value={confirmPassword}
+                                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                                required
+                                            />
+                                        </div>
                                        
-                                        <label className="form-label" htmlFor="typePasswordX">Confirmar Contraseña</label>
-        <button className="btn btn-outline-light btn-lg px-5">Enviar</button>
-        </div>
+                                        
+        <button className="btn btn-outline-light btn-lg px-5" type="submit">Restablecer Contraseña</button>
+   
        </form>
+       {message && <p>{message}</p>}
        </div>
                             </div>
                         </div>
