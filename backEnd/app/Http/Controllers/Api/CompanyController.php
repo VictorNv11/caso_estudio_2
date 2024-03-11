08e011363597f610e9977d25b23b58adad20532f
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-03-11 10:40:16 -0500

    Código aleatorio al crear la compañía

--- backEnd/app/Http/Controllers/Api/CompanyController.php
+++ backEnd/app/Http/Controllers/Api/CompanyController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Exception;
+use App\Models\User;
+
+// Genera los códigos aleatorios
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
@@ -13,8 +19,8 @@ class CompanyController extends Controller
     public function index()
     {
         try {
-            $companies = Company::all();
-            return $companies;
+            $company = Company::all();
+            return $company;
         } catch(Exception $e) {
             return response()->json(['error' => 'Hubo un error al obtener las compañías: ' . $e->getMessage()], 500);
         }
@@ -33,27 +39,30 @@ class CompanyController extends Controller
                 'nit' => 'required|string|max:10',
                 'phone' => 'required|string|max:10',
                 'email' => 'required|email|max:255',
-                'status' => 'required|boolean',
                 'document' => 'nullable|file|mimes:pdf|max:2048', // Ejemplo: solo acepta archivos PDF de hasta 2MB
             ]);
 
-            $companies= new Company();
-            $companies->name_company = $request->name_company;
-            $companies->address = $request->address;
-            $companies->nit = $request->nit;
-            $companies->phone = $request->phone;
-            $companies->email = $request->email;
-            $companies->status = $request->status;
+            // Genera un código de aprobación genérico
+            $approvalCode = Str::random(10);
+
+            // Crea la compañía
+            $company= new Company();
+            $company->name_company = $request->name_company;
+            $company->address = $request->address;
+            $company->nit = $request->nit;
+            $company->phone = $request->phone;
+            $company->email = $request->email;
+            $company->approval_code = $approvalCode;
 
              // Procesar el documento
             if ($request->hasFile('document')) {
                 $file = $request->file('document');
                 $path = $file->store('documents'); // Guarda el archivo en la carpeta 'documents' dentro del almacenamiento de Laravel
-                $companies->document = $path;
+                $company->document = $path;
             }
 
             // Guarda la compañía creada
-            $companies->save();
+            $company->save();
             return response()->json(['message' => 'Compañía creada con éxito'], 201);
 
         } catch(Exception $e) {
@@ -62,15 +71,47 @@ class CompanyController extends Controller
              
     }
 
+    public function approve(Request $request, string $id)
+    {
+        try {
+            // Verifica si el usuario está autenticado y tiene el rol adecuado
+            if (Auth::check()) {
+                // Encuentra la compañía por su ID
+                $company = Company::findOrFail($id);
+            
+
+            if (Auth::user()->isAdmin()) {
+                // Verifica si el código de aprobación proporcionado coincide con el código de la compañía
+                if ($request->approval_code !== $company->approval_code) {
+                    return response()->json(['error' => 'Código de aprobación incorrecto'], 400);
+                }
+
+                // Actualiza el estado de la compañía a "Aprobada"
+                $company->status = true;
+                $company->approval_code = null; // Limpia el código de aprobación
+                $company->save();
+
+                return response()->json(['message' => 'Compañía aprobada con éxito'], 200);
+            } else {
+                return response()->json(['error' => 'No tienes permisos para aprobar compañías'], 403);
+            }
+       } else {
+            return response()->json(['error' => 'Debes iniciar sesión para aprobar compañías'], 401);
+        } 
+    } catch(Exception $e) {
+            return response()->json(['error' => 'Hubo un error al aprobar la compañía: ' . $e->getMessage()], 500);
+        }
+    }
+
     // Leer (Read)
     public function show(string $id)
     {
         try {
-            $companies = Company::find($id);
-            if (!$companies) {
+            $company = Company::find($id);
+            if (!$company) {
                 return response()->json(['error' => 'No se encontró la compañía'], 404);
             }
-            return $companies;
+            return $company;
         } catch(Exception $e) {
             return response()->json(['error' => 'Hubo un error al recuperar la compañía: ' . $e->getMessage()], 500);
         }
@@ -81,7 +122,7 @@ class CompanyController extends Controller
     {
         try {
             // Busca la compañía por su Id
-            $companies = Company::findOrFail($id);
+            $company = Company::findOrFail($id);
 
             // Válida los campos antes de actualizar la compañía
             $request->validate([
@@ -95,10 +136,10 @@ class CompanyController extends Controller
             ]);
 
             // Actualizar solo los campos válidos
-            $companies->fill($request->all());
+            $company->fill($request->all());
 
             // Guardar la compañía actualizada
-            $companies->save();
+            $company->save();
 
             return response()->json(['message' => 'Compañía actualizada con éxito'], 200);
         } catch (Exception $e) {
